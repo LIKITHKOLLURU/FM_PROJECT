19faa3884e1236c0f057db989fcfe140a9dd0e30
KOLLURU LIKITH KUMAR <kollurulikith@gmail.com>
2025-05-10 09:55:56 +0530

    updated push

--- free_backend/src/main/java/com/KLU/repository/JobRepo.java
+++
@@ -1,7 +0,0 @@
-package com.KLU.repository;
-
-import com.KLU.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepo extends JpaRepository<Job, Long> {
-}
